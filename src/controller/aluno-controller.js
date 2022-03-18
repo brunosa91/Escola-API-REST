744a9f18c07618452a31d2e575b48a6ecc8fc887
brunosa91 <brunosantana.engenharia@gmail.com>
2022-03-18 14:53:37 -0300

    organizado as rotas para posteriormente criar a dao

--- src/controller/aluno-controller.js
+++ src/controller/aluno-controller.js
@@ -2,6 +2,22 @@ const alunoController = (app) => {
   app.get("/aluno", async (req, res) => {
     await res.send("foi");
   });
+  //
+  app.get("/aluno/matricula/:matricula", async (req, res) => {
+    await res.send("peguei matricula");
+  });
+
+  app.post("/aluno", async (req, res) => {
+    //vai receber o corpo da requisição;
+  });
+  app.delete("/aluno/id:id", async (req, res) => {
+    //usa o parametro de filtro e depois deleta
+    await res.send("foi");
+  });
+  app.put("/aluno/id:id", async (req, res) => {
+    //usa o parametro de filtro e depois atualiza
+    await res.send("foi");
+  });
 };
 
 export default alunoController;
