5bc5fe6d6e6356c3c38a54bde5cf13175bb32fed
brunosa91 <brunosantana.engenharia@gmail.com>
2022-03-18 16:47:55 -0300

    criado a class Aluno e instanciado a class utilizada na dao e criado os métodos onde suas respostas serão mostradas na controller

---
+++ src/model/Aluno.js
@@ -0,0 +1,18 @@
+import AlunoDao from "../DAO/Aluno_dao.js";
+import AlunoSchema from "./schemas/AlunoSchema.js";
+
+class Aluno {
+  constructor(db) {
+    this.dao = new AlunoDao(db);
+  }
+
+  buscaTodosAlunos = async () => {
+    try {
+      return await this.dao.buscaTodosAlunos();
+    } catch (error) {
+      throw new Error(error.mensagem);
+    }
+  };
+}
+
+export default Aluno;
