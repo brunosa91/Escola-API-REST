7662a3326fa8bf8766a8166b795bf627fa06c8ac
brunosa91 <brunosantana.engenharia@gmail.com>
2022-03-18 14:53:04 -0300

    importado banco de dados e passado com parâmetro para controller

--- src/app.js
+++ src/app.js
@@ -4,13 +4,20 @@ import express from "express";
 
 import alunoController from "./controller/aluno-controller.js";
 
+// IMPORTANDO DATABASE
+
+import db from "./database/sqlite-db.js";
+
 // instanciando meu express
 const app = express();
 
+//instanciando database
+
+const database = db;
 // utilizando o json através do métdo use
 app.use(express.json());
 
 //Chamar aluno controller e passa como parâmetro app
 
-alunoController(app);
+alunoController(app, database);
 export default app;
