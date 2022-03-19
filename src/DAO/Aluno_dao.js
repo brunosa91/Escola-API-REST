7fbd56d77f1b54f0ee407430f018414288dd0dfb
brunosa91 <brunosantana.engenharia@gmail.com>
2022-03-19 00:03:23 -0300

    criado o método para inserir dados no db

--- src/DAO/Aluno_dao.js
+++ src/DAO/Aluno_dao.js
@@ -18,13 +18,41 @@ class AlunoDao {
   buscaMatricula = (matricula) => {
     return new Promise((resolve, reject) => {
       this.db.all(
-        "SELECT * FROM ALUNO WHERE MATRICULA = ?",
+        "SELECT * FROM ALUNOS WHERE MATRICULA = ?",
         matricula,
         (error, rows) => {
           if (error) {
             reject(error);
           } else {
             resolve(rows);
+          }
+        }
+      );
+    });
+  };
+  insereDadosAluno = (novoAluno) => {
+    return new Promise((resolve, reject) => {
+      this.db.run(
+        "INSERT INTO ALUNOS (NOME,DATA_DE_NASCIMENTO, CPF, EMAIL, TELEFONE, TURMA, ANO, MATRICULA, RESPONSAVEL, RUA, NUMERO, CEP, CIDADE, BAIRRO) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+        novoAluno.nome,
+        novoAluno.dataDeNascimento,
+        novoAluno.cpf,
+        novoAluno.email,
+        novoAluno.telefone,
+        novoAluno.turma,
+        novoAluno.ano,
+        novoAluno.matricula,
+        novoAluno.responsavel,
+        novoAluno.rua,
+        novoAluno.numero,
+        novoAluno.cep,
+        novoAluno.cidade,
+        novoAluno.bairro,
+        (error, rows) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(`Aluno ${novoAluno.nome} foi registrado com sucesso`);
           }
         }
       );
