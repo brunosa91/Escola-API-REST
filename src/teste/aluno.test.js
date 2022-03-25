ee6cf04a82121954d14698c7fc4e9837b70d720a
brunosa91 <brunosantana.engenharia@gmail.com>
2022-03-25 11:58:48 -0300

    ajustados testes

--- src/teste/aluno.test.js
+++ src/teste/aluno.test.js
@@ -10,11 +10,36 @@ describe("GET /aluno", () => {
       });
   });
 });
-
+describe("POST /aluno", () => {
+  test("Se o body existir", () => {
+    return request(app)
+      .post("/aluno")
+      .send({
+        nome: "Jean Carlos",
+        dataDeNascimento: "16/11/2011",
+        cpf: "111.222.111-44",
+        email: "jr@gmail.com",
+        telefone: "(11) 2111-1111",
+        turma: "noite",
+        ano: "2",
+        matricula: "151515",
+        responsavel: "Olavo Dumont",
+        rua: "Guapé",
+        numero: "520",
+        cep: "08090-050",
+        cidade: "Teresina",
+        bairro: "Campinho",
+      })
+      .then((response) => {
+        expect(response.statusCode).toBe(201);
+        expect(response.body.alunos).toBeTruthy();
+      });
+  });
+});
 describe("GET /aluno/matricula/:matricula", () => {
   test("se o status é 200", () => {
     return request(app)
-      .get("/aluno/matricula/151515")
+      .get("/aluno/matricula/123456")
       .then((response) => {
         expect(response.statusCode).toBe(200);
       });
@@ -51,7 +76,7 @@ describe("POST /aluno", () => {
 describe("PUT /aluno/id", () => {
   test("Se erro for = false", () => {
     return request(app)
-      .put("/aluno/id/2")
+      .put("/aluno/id/3")
       .send({
         nome: "JOÃO",
         dataDeNascimento: "16/11/2011",
@@ -75,10 +100,10 @@ describe("PUT /aluno/id", () => {
   });
 });
 
-describe("DELETE aluno/id/1", () => {
+describe("DELETE aluno/id/", () => {
   test("Se o status for 202", () => {
     return request(app)
-      .delete("/aluno/id/2")
+      .delete("/aluno/id/3")
       .then((response) => {
         expect(response.statusCode).toBe(202);
       });
